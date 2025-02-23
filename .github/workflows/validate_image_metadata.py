a752ebe1452d413859640e1c4b93e346d12fe5ea
Ryan Hartman <rhartman1239@gmail.com>
2025-02-23 01:13:39 -0800

    pythonnnnnn

--- .github/workflows/validate_image_metadata.py
+++ .github/workflows/validate_image_metadata.py
@@ -6,7 +6,7 @@ from vision.utils.image import validate_image_metadata
 if __name__ == "__main__":
     file = Path(__file__)
     try:
-        validate_image_metadata(file / "../../vision/images")
+        validate_image_metadata(file.parent / "../../vision/vision/images")
     except ValueError as err:
         # Print the error string manually to prevent the stack trace from being logged.
         print(err, file=sys.stderr)
